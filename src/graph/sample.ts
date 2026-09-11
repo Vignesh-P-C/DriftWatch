47136b977efe3ec7c0e290050b6205213d665fbe
Vignesh <vignesh.pavalur@gmail.com>
2026-09-11 15:59:52 +0530

    fix: remove conflict markers reintroduced via GitHub web edit

--- src/graph/sample.ts
+++ src/graph/sample.ts
@@ -8,13 +8,7 @@ class Calculator {
     this.total += n;
   }
 }
-<<<<<<< HEAD
-function useAdd() {
-  return add(2, 3);
-}
-=======
 
 function useAdd() {
   return add(2, 3);
 }
->>>>>>> test-branch-a
